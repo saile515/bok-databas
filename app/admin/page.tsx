80750e7df102e04e2a7303ae9f0fefc3a3fd29cd
saile515 <elias.jorgensen2006@gmail.com>
2023-10-27 21:30:57 +0200

    add authentication cookie

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,27 +1,40 @@
 "use client"
 
-import { useState, FormEvent } from "react"
+import { useState, useEffect, FormEvent } from "react"
+import { getCookie, setCookie } from "cookies-next"
 
 export default function Admin() {
-    const [admin_key, set_admin_key] = useState<string>();
+    const [admin_key, set_admin_key] = useState<string>("");
     const [authorized, set_authorized] = useState<boolean>(false);
+    const [ready, set_ready] = useState<boolean>(false);
 
-    async function handle_validation_request(event?: FormEvent) {
-        console.log("Submit", admin_key);
-        event?.preventDefault();
+    async function handle_validation_request(key: string) {
+        if (!key) return;
 
-        if (!admin_key) return;
-
-        const { valid } = await fetch("/api/validate_admin_key", { method: "POST", body: JSON.stringify({ admin_key: admin_key }) }).then(res => res.json());
+        const { valid } = await fetch("/api/validate_admin_key", { method: "POST", body: JSON.stringify({ admin_key: key }) }).then(res => res.json());
 
         if (valid) {
-            console.log(valid);
+            setCookie("admin_key", key);
             set_authorized(true);
         }
+
+        set_ready(true);
     }
 
-    if (!authorized) return (
-        <form onSubmit={handle_validation_request} className="w-full h-screen bg-zinc-50 dark:bg-zinc-900 text-zinc-900 dark:text-zinc-50 flex justify-center items-center flex-col">
+    useEffect(() => {
+        const cookie = getCookie("admin_key");
+        if (cookie) {
+            handle_validation_request(cookie);
+        } else {
+            set_ready(true);
+        }
+    }, [])
+
+    if (ready && !authorized) return (
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            handle_validation_request(admin_key)
+        }} className="w-full h-screen bg-zinc-50 dark:bg-zinc-900 text-zinc-900 dark:text-zinc-50 flex justify-center items-center flex-col">
             <label htmlFor="admin_key" className="text-lg">Infoga admin nyckeln</label>
             <div>
                 <input type="password" id="admin_key" className="input-field" onChange={(event) => { set_admin_key(event.target.value) }} />
@@ -29,7 +42,10 @@ export default function Admin() {
             </div>
         </form>
     );
-    else return (
+    else if (authorized) return (
         <div></div>
+    );
+    else return (
+        <></>
     )
 }

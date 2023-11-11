8f351a331ad575620417f7fc05cd49eda00b0a58
saile515 <elias.jorgensen2006@gmail.com>
2023-11-11 11:21:46 +0100

    fix caching

--- app/books.ts
+++ app/books.ts
@@ -1,12 +1,27 @@
-import { cache } from "react";
 import prisma from "./prisma";
+import type { Book } from "@prisma/client";
 
-export const revalidate = 60 * 60 * 24;
+const revalidate = 1000 * 60 * 60 * 24;
 
-export const get_books = cache(async () => {
-    return await prisma.book.findMany();
+function cache<T>(fn: () => Promise<T>) {
+    let last_query = performance.now();
+    let cached_data: T;
+
+    return async function get_data() {
+        if (performance.now() - last_query < revalidate && cached_data) {
+            return cached_data;
+        } else {
+            cached_data = await fn();
+            last_query = performance.now();
+            return cached_data;
+        }
+    };
+}
+
+export const get_books = cache<Book[]>(async () => {
+    return prisma.book.findMany();
 });
 
-export const count_books = cache(async () => {
-    return await prisma.book.count();
+export const count_books = cache<number>(async () => {
+    return prisma.book.count();
 });
